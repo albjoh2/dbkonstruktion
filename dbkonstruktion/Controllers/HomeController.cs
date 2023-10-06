83721302186e35e6b95fb9110a83964080f4ae7d
albjoh <aljo940616@gmail.com>
2023-10-06 12:31:43 +0200

    Search kampanj implemented

--- dbkonstruktion/Controllers/HomeController.cs
+++ dbkonstruktion/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
 using dbkonstruktion.Models;
 
 namespace dbkonstruktion.Controllers;
@@ -7,14 +8,27 @@ namespace dbkonstruktion.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int kampanjID)
     {
+        ViewBag.kampanjID = kampanjID;
+        return View();
+    }
+
+    public IActionResult SearchKampanj(int kampanjID)
+    {
+        KampanjModel km = new KampanjModel(_configuration);
+        DataTable kampanjdata = km.GetKampanjer(kampanjID);
+        ViewBag.kampanjdata = kampanjdata;
+        ViewBag.kampanjID = kampanjID;
+
         return View();
     }
 
---
+++ dbkonstruktion/Models/KampanjModel.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace dbkonstruktion.Models
+{
+	public class KampanjModel
+	{
+		private IConfiguration _configuration;
+		private string connectionString;
+
+		public KampanjModel(IConfiguration configuration)
+		{
+			_configuration = configuration;
+			connectionString = _configuration["ConnectionString"];
+		}
+
+		public DataTable GetKampanjer(int kampanjID)
+		{
+			MySqlConnection dbcon = new MySqlConnection(connectionString);
+			dbcon.Open();
+			MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM kampanj WHERE nr=@inputname;", dbcon);
+			adapter.SelectCommand.Parameters.AddWithValue("@inputname", kampanjID);
+			DataSet ds = new DataSet();
+			adapter.Fill(ds, "result");
+			DataTable customerTable = ds.Tables["result"];
+			dbcon.Close();
+
+			return customerTable;
+		}
+	}
+}
+
